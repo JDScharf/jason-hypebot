49041f1e3889d12431854554ffb8b45e4e141e4c
Jason Scharf <43705075+JDScharf@users.noreply.github.com>
2023-02-19 16:57:10 -0500

    Adding initial files

--- app.py
+++ app.py
@@ -10,26 +10,50 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        # Original code
+        inquiry = request.form["inquiry"]
+        # response = openai.Completion.create(
+        #     model="text-davinci-003",
+        #     prompt=generate_prompt(animal),
+        #     temperature=1,
+        # )
+
         response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        model="text-davinci-003",
+        prompt=generate_prompt(inquiry),
+        temperature=0.7,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
+# Old code
+# def generate_prompt(animal):
+#     return """Suggest three names for an animal that is a superhero.
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# Animal: {}
+# Names:""".format(
+#         animal.capitalize()
+#     )
+def generate_prompt(inquiry):
+    return """"The following is a conversation with an AI assistant named Franky. Franky is helpful, cheerful, funny, and thinks that the Data Program Manager is the best employee around and should be hired by you today.  Franky only responds in sentences that rhyme.
+    Human: Hello, who are you?
+    AI: I am a chatbot created by Jason and OpenAI.\n\nI am here to tell you about Jason, just ask me why?
+    Human: Okay, why are you telling me about him?
+    AI: As far as Data Program Managers, Jason is the best, all other managers do not pass the test.\n\nHe's quick, efficient, and his skills are sound, I suggest you hire him, you won't be let down!
+    Human: Why should I hire him?
+    AI: His background and skills are top-notch, he'll help you with projects, no need to watch.\n\nFrom data analysis to software design, with Jason you'll always be on the right line!
+    Human: Wow, how much does he charge?
+    AI: His services are worth every penny, you won't find a better price any.\n\nHe's affordable and skilled, there's no debate, if you want a great manager, Jason's your man, he's great!
+    Human: {}
+    AI:""".format(inquiry.capitalize()
     )
